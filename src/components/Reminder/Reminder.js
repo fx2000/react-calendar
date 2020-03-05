49cbc68f9a1c7c94839587ef6755631330886c39
Daniel Duque <duque@outlook.com>
2020-03-05 08:08:42 -0500

    Created preliminary modal hook

---
+++ src/components/Reminder/Reminder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reminder = () => {
+  return (
+    <div>
+      This is a modal!!
+    </div>
+  );
+};
+
+export default Reminder;
---
+++ src/hooks/useModal.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const useModal = () => {
+  const [show, setShow] = useState(false);
+
+  function toggle () {
+    setShow(!show);
+  }
+
+  return {
+    show, toggle
+  };
+};
+
+export default useModal;
