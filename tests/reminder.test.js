10413bb20259bdd2d91f640568f0f8482ff09a4c
Daniel Duque <duque@outlook.com>
2020-03-11 13:17:05 -0500

    Preliminary unit tests

--- tests/reminder.test.js
+++ tests/reminder.test.js
@@ -1,22 +1,33 @@
-const request = require('supertest');
 const app = require('../app.js');
+const supertest = require('supertest');
+const request = supertest(app);
+const { parseJSON, parseISO } = require('date-fns')
 
 describe('Test API Endpoints', () => {
-  it('Create a new reminder', async () => {
+  it('Create a new reminder', async done => {
     const validReminder = {
       description: 'This is a sample reminder',
       city: 'Panama City',
       color: '#FF3300',
-      datetime: '1989-12-20T06:00:000Z'
-    }; 
-    const res = await request(app)
-      .post('/api/create')
-      .send(validReminder)
-    expect(res.statusCode).toEqual(200)
+      datetime: '1989-12-20T06:00:00.000Z'
+    };
+    const res = await request.post('/api/create').send(validReminder)
+
+    expect(res.statusCode).toBe(200)
     expect(res.body._id).toBeDefined();
     expect(res.body.description).toBe(validReminder.description);
     expect(res.body.city).toBe(validReminder.city);
     expect(res.body.color).toBe(validReminder.color);
     expect(res.body.datetime).toBe(validReminder.datetime);
-  })
+
+    done();
+  });
+
+  it('List all reminders', async done => {
+    const res = await request.get('/api');
+
+    expect(res.statusCode).toBe(200);
+
+    done();
+  });
 })
