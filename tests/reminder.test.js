65fff99ac24de928b8abff1ca8deb27c43a96012
Daniel Duque <duque@outlook.com>
2020-03-12 17:06:07 -0500

    Added aditional tests

--- tests/reminder.test.js
+++ tests/reminder.test.js
@@ -2,6 +2,7 @@ const app = require('../app.js');
 const supertest = require('supertest');
 const request = supertest(app);
 
+// TODO: Set up and seed a test database to improve API endpoint testing
 describe('Test API Endpoints', () => {
   it('Create a new reminder', async done => {
     const validReminder = {
@@ -18,6 +19,42 @@ describe('Test API Endpoints', () => {
     expect(res.body.city).toBe(validReminder.city);
     expect(res.body.color).toBe(validReminder.color);
     expect(res.body.datetime).toBe(validReminder.datetime);
+
+    done();
+  });
+
+  // TODO: Come up with a better test for this
+  it('List all reminders', async done => {
+    const res = await request.get('/api');
+
+    expect(res.statusCode).toBe(200);
+
+    done();
+  });
+
+  it('Get a specific reminder', async done => {
+    // Create a new reminder
+    const validReminder = {
+      description: 'This is a sample reminder',
+      city: 'Panama City',
+      color: '#FF3300',
+      datetime: '1989-12-20T06:00:00.000Z'
+    };
+    const res = await request.post('/api/create').send(validReminder);
+
+    // Capture new reminder's id
+    const newReminderId = res.body._id;
+
+    //  Get reminder information
+    const resUpdate = await request.get(`/api/${newReminderId}`);
+
+    expect(resUpdate.statusCode).toBe(200);
+    expect(resUpdate.body._id).toBe(newReminderId);
+    expect(resUpdate.body.description).toBe(validReminder.description);
+    expect(resUpdate.body.city).toBe(validReminder.city);
+    expect(resUpdate.body.color).toBe(validReminder.color);
+    expect(resUpdate.body.datetime).toBe(validReminder.datetime);
+    expect(resUpdate.body.deleted).toBe(false);
 
     done();
   });
@@ -44,7 +81,6 @@ describe('Test API Endpoints', () => {
     };
     const resUpdate = await request.put(`/api/update/${newReminderId}`).send(updatedReminder);
 
-    // Test
     expect(resUpdate.statusCode).toBe(200);
     expect(resUpdate.body._id).toBe(newReminderId);
     expect(resUpdate.body.description).toBe(updatedReminder.description);
@@ -55,12 +91,39 @@ describe('Test API Endpoints', () => {
     done();
   });
 
+  it('Mark an existing reminder as deleted', async done => {
+    // Create a new reminder
+    const validReminder = {
+      description: 'This is a sample reminder',
+      city: 'Panama City',
+      color: '#FF3300',
+      datetime: '1989-12-20T06:00:00.000Z'
+    };
+    const res = await request.post('/api/create').send(validReminder);
+
+    // Capture new reminder's id
+    const newReminderId = res.body._id;
+
+    // Mark reminder as deleted
+    const resDeleted = await request.get(`/api/delete/${newReminderId}`);
+
+    expect(resDeleted.statusCode).toBe(200);
+    expect(resDeleted.body._id).toBe(newReminderId);
+    expect(resDeleted.body.deleted).toBe(true);
+
+    done();
+  });
+
+  it('Mark all reminders in a specific date as deleted', async done => {
+    done();
+  });
+
   // TODO: Come up with a better test for this
-  it('List all reminders', async done => {
-    const res = await request.get('/api');
+  it('List all reminders in a specific date', async done => {
+    const date = '1989-12-20';
+    const res = await request.get(`/api/date/${date}`);
 
     expect(res.statusCode).toBe(200);
-
     done();
   });
 })
