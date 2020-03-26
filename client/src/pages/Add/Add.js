97b901d73ed2270600486df5089b9fd9e4e3b588
Daniel Duque <duque@outlook.com>
2020-03-26 14:27:37 -0500

    Migrated add form to react-form-hook

--- client/src/pages/Add/Add.js
+++ client/src/pages/Add/Add.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useInput } from '../../hooks/input-hook';
+import { useForm } from 'react-hook-form';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -9,46 +9,27 @@ import remindersApi from '../../lib/APIservice';
 export const Add = () => {
   const [startDate, setStartDate] = useState(new Date());
 
-  const {
-    value: description,
-    bind: bindDescription,
-    reset: resetDescription
-  } = useInput('');
-  const {
-    value: city,
-    bind: bindCity,
-    reset: resetCity
-  } = useInput('');
-  const {
-    value: color,
-    bind: bindColor,
-    reset: resetColor
-  } = useInput('#99FFFF');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
     const newReminder = {
-      description: description,
-      city: city,
-      color: color,
+      description: data.description,
+      city: data.city,
+      color: data.color,
       datetime: startDate
     };
     console.log(newReminder);
-    remindersApi.create(newReminder);
-    resetColor('#99FFFF');
-    resetDescription('');
-    resetCity('');
+    remindersApi.create(newReminder).then(window.location.href = '/');
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='description'>Reminder: </label>
-        <input type='text' id='description' name='description' {...bindDescription}></input>
+        <input type='text' id='description' name='description' defaultValue="test" ref={register({ required: true })}></input>
         <label htmlFor='city'>City: </label>
-        <input type='text' id='city' name='city' {...bindCity}></input>
+        <input type='text' id='city' name='city' defaultValue="test" ref={register({ required: true })}></input>
         <label htmlFor='color'>Color: </label>
-        <input type='color' id='color' name='color' {...bindColor}></input>
+        <input type='color' id='color' name='color' defaultValue="test" ref={register({ required: true })}></input>
         <label htmlFor='datetime'>Date & Time: </label>
         <DatePicker
           selected={startDate}
@@ -59,6 +40,7 @@ export const Add = () => {
           timeCaption="Time"
           dateFormat="MMMM d, yyyy h:mm aa"
         />
+        {errors.exampleRequired && <span>This field is required</span>}
         <button type='submit'>Submit</button>
       </form>
     </div>
