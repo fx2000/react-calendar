1150505d42920b889a5eaee26bab9afd60949ef0
Daniel Duque <duque@outlook.com>
2020-04-13 17:46:09 -0500

    More heroku tweaks

--- app.js
+++ app.js
@@ -37,7 +37,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
+// Production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'client/build/index.html'));
+  });
+};
+// Build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+});
 
 // Allow frontend access to API (CORS)
 app.use(cors());
