f24fda6720c0ec925553ec6429b4b9764602dc95
Daniel Duque <duque@outlook.com>
2020-04-13 17:56:31 -0500

    More heroku tweaks

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 // Allow frontend access to API (CORS)
